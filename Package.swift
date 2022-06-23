8832e2982878da4b0179d65a7894c251a55864db
Rory Kelly <rory@rockspin.co.uk>
2022-06-23 14:19:23 +0100

    removing dependencies

--- Package.swift
+++ Package.swift
@@ -18,22 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Cabbage",
-            dependencies: [],
-            linkerSettings: [
-                .linkedFramework(name: "AVFoundation", 
-            ]
-        ),
-            linkerSettings: [
-                .linkedFramework(
-                    "AVFoundation",
-                    .when(platforms: [.iOS, .macOS])
-                ),
-                .linkedFramework(
-                    "Foundation",
-                    .when(platforms: [.iOS, .macOS])
-                )
-            ]
+        .target(name: "Cabbage", dependencies: [])
     ]
 )
